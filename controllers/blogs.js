988f7d2c0d968ccfe8fddefed941c4fabee047de
iulover99 <iulover99@gmail.com>
2020-07-05 03:21:10 +0700

    part 4 done completedly

---
+++ controllers/blogs.js
@@ -0,0 +1,83 @@
+const jwt = require('jsonwebtoken')
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+/*
+  We don't need next(exception) call because library 'express-async-errors' handles everything for us
+*/
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({}).populate('user',{ username: 1, name: 1 })
+  response.json(blogs.map(blog=> blog.toJSON()))
+})
+
+blogsRouter.get('/:id', async(request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if(blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.post('/', async(request, response) => {
+  const body = request.body
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response
+    .status(201)
+    .json(savedBlog.toJSON())
+})
+
+
+blogsRouter.delete('/:id', async(request, response) => {
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  const deleteBlog = await Blog.findById(request.params.id)
+
+  if(user._id.toString() === deleteBlog.user.toString()){
+    await Blog.findByIdAndRemove(request.params.id)
+    user.blogs = user.blogs.filter(blog => blog.toString() !== request.params.id)
+    await user.save()
+    response.status(204).end()
+  }
+})
+
+blogsRouter.put('/:id', async(request,response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updateBlog = await Blog.findByIdAndUpdate(request.params.id,blog, { new: true })
+  response.json(updateBlog)
+})
+
+module.exports = blogsRouter
---
+++ models/blog.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+// mongoose.set('useFindAndModify', false)
+
+const blogSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: String,
+  url: {
+    type: String,
+    required: true
+  },
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+})
+
+blogSchema.set('toJSON',{
+  transform: (document,returnObject) => {
+    returnObject.id = returnObject._id.toString()
+    delete returnObject._id
+    delete returnObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog',blogSchema)
